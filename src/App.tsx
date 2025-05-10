7f9d9e1ff1eb86db39f0f22b95ce69fd3bdc3abb
rockit456 <zbeidas@icloud.com>
2025-05-09 22:45:05 -0400

    added container

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import './App.css';
 import CartoonButton from './components/buttons/CartoonButton';
+import CartoonContainer from './components/containers/CartoonContainer';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <div style={{ marginTop: '2rem', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-          {/* All of this can be removed i am just testing */}
-          <CartoonButton color="#FF6B6B">
-            Register for Donations
-          </CartoonButton>
+        <CartoonContainer color="#FFFFFF">
+          <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+            {/* All of this can be removed i am just testing */}
+            <CartoonButton color="#FF6B6B">
+              Register for Donations
+            </CartoonButton>
 
-          <CartoonButton color="#4ECDC4">
-            Register to Sponsor
-          </CartoonButton>
+            <CartoonButton color="#4ECDC4">
+              Register to Sponsor
+            </CartoonButton>
 
-          <CartoonButton color="#FFE66D">
-            Donate Now!
-          </CartoonButton>
-        </div>
+            <CartoonButton color="#FFE66D">
+              Donate Now!
+            </CartoonButton>
+          </div>
+        </CartoonContainer>
       </header>
     </div>
   );
---
+++ src/components/containers/CartoonContainer.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface CartoonContainerProps {
+  children: React.ReactNode;
+  className?: string;
+  color?: string;
+}
+
+interface ContainerProps {
+  color: string;
+}
+
+const Container = styled.div<ContainerProps>`
+  position: relative;
+  padding: 24px;
+  background-color: ${props => props.color};
+  border: 3px solid #0f1418;
+  border-radius: 10px;
+  box-shadow: 4px 4px 0 0 #0f1418;
+`;
+
+const CartoonContainer: React.FC<CartoonContainerProps> = ({ 
+  children, 
+  className,
+  color = "#FFFFFF"
+}) => {
+  return (
+    <Container
+      className={className}
+      color={color}
+    >
+      {children}
+    </Container>
+  );
+};
+
+export default CartoonContainer;
