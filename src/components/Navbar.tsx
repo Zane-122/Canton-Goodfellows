2b37dfdd0b6df875a1fcaf9c7ffe723db97f4074
rockit456 <zbeidas@icloud.com>
2025-05-10 23:17:18 -0400

    Added the stuff and fixed the navbar again

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,25 @@ import './Navbar.css';
 import logo from '../images/logo.png';
 import CartoonButton from './buttons/CartoonButton';
 import GoogleSignIn from './auth/GoogleSignIn';
-import Container from './containers/CartoonContainer';
+import styled from 'styled-components';
+
+const NavbarContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+  background-color: #FFFFFF;
+  border: 3px solid #0f1418;
+  border-radius: 0;
+  box-shadow: 0 4px 0 0 #0f1418;
+  padding: 1rem 2rem;
+  margin: 0;
+`;
+
 const Navbar: React.FC = () => {
   return (
-    <Container className="navbar-container">
+    <NavbarContainer>
       <nav className="navbar">
         <div className="navbar-logo">
           <CartoonButton color="#ffffff" onClick={() => window.open('https://cantongoodfellows.org/', '_blank')}>
@@ -34,7 +49,7 @@ const Navbar: React.FC = () => {
           </li>
         </ul>
       </nav>
-    </Container>
+    </NavbarContainer>
   );
 };
 
